6a5eccb07d22aef44415e7825eaca396fd9be8af
Daniel C <bitclaw@gmail.com>
2016-05-12 08:36:29 -0500

    Modify test

--- src/client/app/people/people.controller.spec.js
+++ src/client/app/people/people.controller.spec.js
@@ -1,26 +1,33 @@
 /* jshint -W117, -W030 */
 describe('PeopleController', function() {
-  var controller;
-  var people = mockData.getMockPeople();
+     var controller;
+     var people = mockData.getMockPeople();
 
   beforeEach(function() {
-    bard.appModule('app.people');
-    bard.inject('$controller', '$log', '$q', '$rootScope', 'dataservice');
-    controller = $controller('PeopleController');
+      bard.appModule('app.people');
+      bard.inject('$controller', '$log', '$q', '$rootScope');
+
+      var ds - {
+          getPeople: function() {
+              return $q.when(people);
+          }
+      };
+      controller = $controller('PeopleController',{
+          dataservice: ds
+      });
   });
 
   it('should exist',function() {
-    expect(controller).to.exist;
+      expect(controller).to.exist;
   });
 
   it('should have empty people array before activation',function() {
-    expect(controller.people).to.exist;
+      expect(controller.people).to.exist;
   });
 
   it('should have people after activation',function() {
-    expect(controller.people).to.have.length.above(0);
+      $rootScope.$apply;
+      expect(controller.people).to.have.length.above(0);
   });
-
-
 
 });
