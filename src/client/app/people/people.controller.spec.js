6111f3248c98463adf2c0814e60311cf4ec25461
Daniel Chavez <bitclaw@gmail.com>
2016-05-23 15:59:07 -0500

    Add fix for test.

--- src/client/app/people/people.controller.spec.js
+++ src/client/app/people/people.controller.spec.js
@@ -1,49 +1,44 @@
 /* jshint -W117, -W030 */
 describe('PeopleController', function() {
-    var controller;
-    var people = mockData.getMockPeople();
+  var controller;
+  var people = mockData.getMockPeople();
 
-    beforeEach(function() {
+  beforeEach(function() {
+    bard.appModule('app.people');
+    bard.inject(this, '$controller', '$log', '$q', '$rootScope' , 'dataservice');
 
-        bard.appModule('app.people');
-        bard.inject('$controller', '$log', '$q', '$rootScope');
+    var ds = {
+      getPeople: function() {
+        return $q.when(people);
+      }
+    };
 
-        var ds = {
-            getPeople: function() {
-                return $q.when(people);
-            }
-        };
-
-        controller = $controller('PeopleController',{
-            dataservice: ds
-        });
-
+    controller = $controller('PeopleController',{
+      dataservice: ds
     });
 
-    it('should exist',function() {
-        expect(controller).to.exist;
+  });
+
+  it('should exist',function() {
+    expect(controller).to.exist;
+  });
+
+  it('should have empty people array before activation',function() {
+    expect(controller.people).to.exist;
+  });
+
+  describe('after activation', function() {
+    it('should have people',function() {
+      $rootScope.$apply;
+      expect(controller.people).to.have.length.above(0);
     });
 
-    it('should have empty people array before activation',function() {
-        expect(controller.people).to.exist;
+    it('should have mock people',function() {
+      $rootScope.$apply;
+      console.debug(people);
+      console.debug(controller);
+      expect(controller.people).to.have.length(people.length);
     });
-
-    describe('after activation', function() {
-
-        beforeEach(function() {
-            $rootScope.$apply;
-        });
-
-        it('should have people',function() {
-            $rootScope.$apply;
-            expect(controller.people).to.have.length.above(0);
-        });
-
-        it('should have mock people',function() {
-            $rootScope.$apply;
-            console.debug(controller.people);
-            expect(controller.people).to.have.length(1);
-        });
-    });
+  });
 
 });
