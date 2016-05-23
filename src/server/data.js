59a1f5f1c5dbba8b52390628ddd050f2115e59df
Daniel Chavez <bitclaw@gmail.com>
2016-05-23 14:34:33 -0500

    Add changes to directive.

--- src/server/data.js
+++ src/server/data.js
@@ -14,3 +14,19 @@ function getPeople() {
     { id: 8, firstName: 'Aaron', lastName: 'Jinglehiemer', age: 22, location: 'Utah' }
   ];
 }
+
+// function getPeople() {
+//   return {
+//     name: 'Luke Skywalker',
+//     address: {
+//       street: 'PO Box 123',
+//       city: 'A secret rebel base.',
+//       planet: 'Yavin 4'
+//     },
+//     friends: [
+//       'Han',
+//       'Leia',
+//       'Chewbacca'
+//     ]
+//   };
+// }
